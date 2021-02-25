1e9a26cb3f9b0a6b50aeb1dc6b054c5e1cd969a9
mgajda83 <47522674+mgajda83@users.noreply.github.com>
2021-02-25 15:55:33 +0100

    Add files via upload
    Upload base module

---
+++ PSSentinelOneAPI/Invoke-PSSentinelOneApi.ps1
@@ -0,0 +1,30 @@
+Function Invoke-PSSentinelOneApi
+{
+    [CmdletBinding()]
+	param 
+	(
+		[Parameter(Mandatory = $true)]
+		[Hashtable]$Request
+	)
+
+    $Response = Invoke-RestMethod @Request
+    
+    $Data = @()
+    $Data += $Response.data
+    $Counter = $Data.Count
+    $Total = $Response.pagination.totalItems
+    
+	#Paginacja
+    While($Data.Count -lt $Total)
+    {
+		Write-Progress -Activity "Przetwarzanie danych" -status "$Counter / $Total" -percentcomplete $([Int](($Counter/$Total)*100))
+	
+        $Request = New-PSSentinelOneApiRequest -Uri $Request.Uri -Method GET -Headers $Request.Headers -Filter @{"cursor"=$Response.pagination.nextCursor}
+		
+        $Response = Invoke-RestMethod @Request
+        $Data += $Response.data
+        $Counter = $Data.Count
+    }
+
+	return $Data
+}
---
+++ PSSentinelOneAPI/New-PSSentinelOneApiRequest.ps1
@@ -0,0 +1,57 @@
+Function New-PSSentinelOneApiRequest
+{
+    [CmdletBinding()]
+	param 
+	(
+		[Parameter(Mandatory = $true,ParameterSetName="ApiToken")]
+		[String]$ApiToken,
+		[Parameter(Mandatory = $true)]
+		[ValidateSet("GET", "POST", "PUT", "DELETE")]
+		[String]$Method,
+		[Parameter(Mandatory = $true)]
+		[String]$Uri,
+        [Parameter(Mandatory = $true,ParameterSetName="Headers")]
+        [Hashtable]$Headers,
+		[Parameter()]
+		[String]$Body,
+		[Parameter()]
+		[Hashtable]$Filter
+	)
+
+    #Buduj parametryzacje dla GET'a
+	Add-Type -AssemblyName System.Web
+	$QueryString = [System.Web.HttpUtility]::ParseQueryString([String]::Empty)
+	
+	if($null -ne $Filter)
+	{
+		ForEach($Item in $Filter.GetEnumerator())
+		{ 
+			$QueryString.Add($Item.Name, $Item.Value) 
+		}		
+	}
+	
+	$UriRequest = [System.UriBuilder]$Uri
+	$UriRequest.Query = $QueryString.ToString()
+
+  	#Buduj naglowki
+    if(!$Headers)
+    {
+        $Headers = @{}
+        $Headers.Add("Authorization","ApiToken $ApiToken")
+    }
+
+	#Buduj request
+	$Request = @{}
+    $Request.Add("Method",$Method)
+    $Request.Add("Headers",$Headers)
+    $Request.Add("Uri",$UriRequest.Uri.OriginalString)
+    Write-Verbose $UriRequest.Uri.OriginalString
+
+    if($Body) 
+    { 
+        $Request.Add("Body",$Body) 
+        $Request.Add("ContentType", "application/json")
+    }
+
+    return $Request
+}
