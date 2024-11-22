60410d89216411bae26166111ef99fa21fcd74af
Michal Gajda <47522674+mgajda83@users.noreply.github.com>
2024-11-22 12:18:01 +0100

    Update Invoke-PSSentinelOneApi.ps1

--- PSSentinelOneAPI/Invoke-PSSentinelOneApi.ps1
+++ PSSentinelOneAPI/Invoke-PSSentinelOneApi.ps1
@@ -1,54 +1,65 @@
 Function Invoke-PSSentinelOneApi
 {
-	<#
-	.SYNOPSIS
-		Invoke SentineOne API request.
+        <#
+        .SYNOPSIS
+                Invoke SentineOne API request.
 
-	.PARAMETER Request
-		Request object.
+        .PARAMETER Request
+                Request object.
 
-	.PARAMETER Silent
-		Witout Write-Progress
+        .PARAMETER Sleep
+                Seconds for start sleep, to wait after limit reached.
+		
+        .PARAMETER Silent
+                Witout Write-Progress
 
-	.EXAMPLE
-		$Request = New-PSSentinelOneApiRequest -Uri "https://euce1-103.sentinelone.net/web/api/v2.1/agents" -Method GET -ApiToken $ApiToken
-		$Agents = Invoke-PSSentinelOneApi -Request $Request
+        .EXAMPLE
+                $Request = New-PSSentinelOneApiRequest -Uri "https://euce1-103.sentinelone.net/web/api/v2.1/agents" -Method GET -ApiToken $ApiToken
+                $Agents = Invoke-PSSentinelOneApi -Request $Request
 
-	.NOTES
-		Author: Michal Gajda
-	#>
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory = $true)]
-		[Hashtable]$Request,
-		[Parameter()]
-		[Switch]$Silent
-	)
+        .NOTES
+                Author: Michal Gajda
+        #>
+        [CmdletBinding()]
+        param
+        (
+                [Parameter(Mandatory = $true)]
+                [Hashtable]$Request,
+		[Int]$Sleep,
+                [Parameter()]
+                [Switch]$Silent
+        )
 
-	Write-Verbose $Request.Uri
-	$Response = Invoke-RestMethod @Request
+        Write-Verbose $Request.Uri
+        $Response = Invoke-RestMethod @Request
 
-	$Data = @()
-	$Data += $Response.data
-	$Counter = $Data.Count
-	$Total = $Response.pagination.totalItems
+        $Data = @()
+        $Data += $Response.data
+        $Counter = $Data.Count
+        $Total = $Response.pagination.totalItems
 
-	#Pagination
-	While($Response.pagination.nextCursor)
-	{
-		if(!$Silent.IsPresent)
-		{
-			Write-Progress -Activity "Przetwarzanie danych" -status "$Counter / $Total" -percentcomplete $([Int](($Counter/$Total)*100))
+        #Pagination
+        While($Response.pagination.nextCursor)
+        {
+                if(!$Silent.IsPresent)
+                {
+                        Write-Progress -Activity "Przetwarzanie danych" -status "$Counter / $Total" -percentcomplete $([Int](($Counter/$Total)*100))
+                }
+
+        	try {
+                	$Request = New-PSSentinelOneApiRequest -Uri $Request.Uri -Method $Request.Method -Headers $Request.Headers -Body @{"cursor"=$Response.pagination.nextCursor}
+
+                	Write-Verbose $Request.Uri
+                	$Response = Invoke-RestMethod @Request
+                	$Data += $Response.data
+                	$Counter = $Data.Count
 		}
+		catch
+		{
+            		Write-Verbose "$($_.Exception.Message). Start sleep $($Sleep)s"
+            		Start-Sleep -Seconds $Sleep
+		}
+        }
 
-		$Request = New-PSSentinelOneApiRequest -Uri $Request.Uri -Method $Request.Method -Headers $Request.Headers -Body @{"cursor"=$Response.pagination.nextCursor}
-
-		Write-Verbose $Request.Uri
-		$Response = Invoke-RestMethod @Request
-		$Data += $Response.data
-		$Counter = $Data.Count
-	}
-
-	return $Data
+        return $Data
 }
